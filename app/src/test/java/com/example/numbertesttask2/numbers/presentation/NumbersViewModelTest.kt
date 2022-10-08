1ce6afc72a2c883141531e22138315ef4963520c
KruzhokProg <Aa1@3$5^7>
2022-10-08 11:15:48 +0300

    tdd

---
+++ app/src/test/java/com/example/numbertesttask2/numbers/presentation/NumbersViewModelTest.kt
@@ -0,0 +1,155 @@
+package com.example.numbertesttask2.numbers.presentation
+
+import org.junit.Assert.*
+import org.junit.Test
+
+class NumbersViewModelTest {
+
+    /**
+     * Initial test
+     * At start fetch data and show it
+     * then try to get some data
+     * then re-init and check the result
+     */
+    @Test
+    fun `test init and re-init`() {
+        val communications = TestNumberCommunications()
+        val interactor = TestNumbersInteractor()
+
+        val viewModel = NumbersViewModel(communications, interactor)
+        interactor.changeExpectedResult(NumbersResult.Success())
+
+        viewModel.init(isFirstRun = true)
+
+        assertEquals(1, communications.progressCalledList.size)
+        assertEquals(true, communications.progressCalledList[0])
+
+        assertEquals(2, communications.progressCalledList.size)
+        assertEquals(false, communications.progressCalledList[1])
+
+        assertEquals(1, communications.stateCalledList.size)
+        assertEquals(UiState.Success(), communications.stateCalledList[0])
+
+        assertEquals(0, communications.numbersList.size)
+        assertEquals(1, communications.timesShowList)
+
+        // get some data
+        interactor.changeExpectedResult(NumbersResult.Failure())
+        viewModel.fetchRandomNumberData()
+
+        assertEquals(3, communications.progressCalledList.size)
+        assertEquals(true, communications.progressCalledList[2])
+
+        assertEquals(1, interactor.fetchAboutRandomNumberCalledList.size)
+
+        assertEquals(4, communications.progressCalledList.size)
+        assertEquals(false, communications.progressCalledList[3])
+
+        assertEquals(2, communications.stateCalledList.size)
+        assertEquals(UiState.Error("/* todo message */"), communications.stateCalledList[1])
+        assertEquals(1, communications.timesShowList)
+
+        viewModel.init(isFirstRun = false)
+        assertEquals(4, communications.progressCalledList.size)
+        assertEquals(2, communications.stateCalledList.size)
+        assertEquals(1, communications.timesShowList)
+    }
+
+
+    /**
+     *
+     */
+    @Test
+    fun `fact about empty number`(){
+        val communications = TestNumberCommunications()
+        val interactor = TestNumbersInteractor()
+
+        val viewModel = NumbersViewModel(communications, interactor)
+
+        viewModel.fetchFact("")
+
+        assertEquals(0, interactor.fetchAboutNumberCalledList.size)
+
+        assertEquals(0, communications.progressCalledList.size)
+
+        assertEquals(1, communications.stateCalledList.size)
+        assertEquals(UiState.Error("entered number is empty"), communications.stateCalledList[0])
+
+        assertEquals(0, communications.timesShowList)
+    }
+
+    @Test
+    fun `fact about some number`(){
+        val communications = TestNumberCommunications()
+        val interactor = TestNumbersInteractor()
+
+        val viewModel = NumbersViewModel(communications, interactor)
+
+        interactor.changeExpectedResult(NumbersResult.Success(Number("45", "fact about 45")))
+        viewModel.fetchFact("45")
+
+        assertEquals(1, communications.progressCalledList.size)
+        assertEquals(true, communications.progressCalledList[0])
+
+        assertEquals(1, interactor.fetchAboutNumberCalledList.size)
+        assertEquals(Number("45", "fact about 45"), interactor.fetchAboutNumberCalledList[0])
+
+        assertEquals(2, communications.progressCalledList.size)
+        assertEquals(false, communications.progressCalledList[1])
+
+        assertEquals(1, communications.stateCalledList.size)
+        assertEquals(UiState.Success(), communications.stateCalledList[0])
+
+        assertEquals(1, communications.timesShowList)
+        assertEquals(NumberUi("45", "fact about 45"), communications.numbersList[0])
+    }
+
+    private class TestNumberCommunications: NumbersCommunications{
+
+        val progressCalledList = mutableListOf<Boolean>()
+        val stateCalledList = mutableListOf<Boolean>()
+        var timesShowList = 0
+        val numbersList = mutableListOf<NumberUi>()
+
+        override fun showProgress(show: Boolean){
+            progressCalledList.add(show)
+        }
+
+        override fun showState(state: UiState){
+            stateCalledList.add(state)
+        }
+
+        override fun showList(list: List<NumberUi>){
+            timesShowList++
+            numbersList.addAll(list)
+        }
+    }
+
+    private class TestNumbersInteractor: NumbersInteractor{
+
+        private var result: NumbersResult = NumbersResult.Success()
+
+        val initCalledList = mutableListOf<NumberResult>()
+        val fetchAboutNumberCalledList = mutableListOf<NumberResult>()
+        val fetchAboutRandomNumberCalledList = mutableListOf<NumberResult>()
+
+        fun changeExpectedResult(newResult: NumbersResult){
+            result = newResult
+        }
+
+        override suspend fun init(): NumbersResult{
+            initCalledList.add(result)
+            return result
+        }
+
+        override suspend fun factAboutNumber(number: String): NumbersResult{
+            fetchAboutNumberCalledList.add(result)
+            return result
+        }
+
+        override suspend fun factAboutRandomNumber(number: String): NumbersResult{
+            fetchAboutRandomNumberCalledList.add(result)
+            return result
+        }
+    }
+}
