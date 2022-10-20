a8b4e3ffbbf4873fef3bd65df2c86fb66e933cf0
KruzhokProg <Aa1@3$5^7>
2022-10-20 16:32:28 +0300

    tdd: repository test

---
+++ app/src/main/java/com/example/numbertesttask2/numbers/data/BaseNumbersRepository.kt
@@ -0,0 +1,7 @@
+package com.example.numbertesttask2.numbers.data
+
+import com.example.numbertesttask2.numbers.domain.NumbersRepository
+
+//class BaseNumbersRepository: NumbersRepository {
+//
+//}
